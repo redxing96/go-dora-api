6c649f4015b28f876d3db1c7733961412cce968c
redxing <redxing96@163.com>
2025-06-26 21:16:09 +0800

    更新代码推送脚本，添加颜色输出以增强可读性，修改最后编辑时间

--- codePush.sh
+++ codePush.sh
@@ -3,15 +3,20 @@
  # @Description: 推送代码到gitee
  # @Author: redxing96@163.com
  # @Date: 2025-06-26 19:12:22
- # @LastEditTime: 2025-06-26 19:40:55
+ # @LastEditTime: 2025-06-26 21:12:35
  # @LastEditors: front end cabbage
  # @FilePath: /go-dora-api/codePush.sh
 ### 
 
+# 定义颜色
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m' # 无色
 
-echo "开始推送代码到gitee"
+echo -e "${YELLOW}开始推送代码到gitee${NC}"
 git push gitee develop
-# echo "开始推送代码到github"
+# echo -e "${YELLOW}开始推送代码到github${NC}"
 # git push github main
-echo "代码推送完成"
+echo -e "${GREEN}代码推送完成${NC}"
 
